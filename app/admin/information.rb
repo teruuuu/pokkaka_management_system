2bcfdb73c2d96e8f56989f91edd7fa0abdb001e1
teru <titlt.aiw6@gmail.com>
2022-01-30 10:25:45 +0900

    informatin内容は修正できないよう修正

--- app/admin/information.rb
+++ app/admin/information.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register Information do
-  permit_params :comment
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
